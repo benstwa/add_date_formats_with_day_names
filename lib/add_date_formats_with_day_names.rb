42a63eb7b92f59380438ea221648c6218ea9b540
Ben Walker <ben.walker@thinktribe.com>
2022-07-22 16:48:08 +0100

    Add a format that includes the day of the week

--- lib/add_date_formats_with_day_names.rb
+++ lib/add_date_formats_with_day_names.rb
@@ -2,7 +2,7 @@ module AddDateFormatsWithDayNames
   def self.included(base)
     base.class_eval do
       def date_format_setting_options(locale)
-        date_formats = ['%a., %d.%m.%Y', '%A, %d.%m.%Y']
+        date_formats = ['%a., %d.%m.%Y', '%A, %d.%m.%Y', '%A, %d %B %Y']
         (Setting::DATE_FORMATS + date_formats).map do |f|
           today = ::I18n.l(User.current.today, :locale => locale, :format => f)
           format = f.delete('%').gsub(/[dmY]/) do
